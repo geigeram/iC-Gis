78e8629b162dd6685b93d40bcd885c91d3e36198
icgeiger <151460645+geigeram@users.noreply.github.com>
2025-09-29 16:57:48 +0200

    init

--- Home.py
+++ Home.py
@@ -1,40 +1,43 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
+import pandas as pd
+
+st.title("Bohrlöcher")
+df = pd.read_csv('data/230710_GeoDatenbank_Lage.csv', sep=';')
+df['lat'] = df['lat'].str.replace(',', '.').astype(float)
+df['lon'] = df['lon'].str.replace(',', '.').astype(float)
+
+projekt_options = ["All"] + list(df['Projektname'].unique())
+selected_projekt = st.selectbox("Select Projektname", projekt_options)
+
+if selected_projekt == "All":
+    filtered_df = df
+else:
+    filtered_df = df[df['Projektname'] == selected_projekt]
+
 
 st.set_page_config(layout="wide")
 
-# Customize the sidebar
-markdown = """
-A Streamlit map template
-<https://github.com/opengeos/streamlit-map-template>
-"""
 
 st.sidebar.title("About")
-st.sidebar.info(markdown)
-logo = "https://i.imgur.com/UbOXYAU.png"
+
+logo = r'C:\Users\a.geiger\Documents\GitHub\streamlit_gis\gis.png'
 st.sidebar.image(logo)
 
-# Customize page title
-st.title("Streamlit for Geospatial Applications")
+m = leafmap.Map(center=[54, 15], zoom=4)
+#cities = pd.read_csv('data/230710_GeoDatenbank_Lage.csv', sep=';')
+        #regions = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_regions.geojson"
 
-st.markdown(
-    """
-    This multipage app template demonstrates various interactive web apps created using [streamlit](https://streamlit.io) and [leafmap](https://leafmap.org). It is an open-source project and you are very welcome to contribute to the [GitHub repository](https://github.com/opengeos/streamlit-map-template).
-    """
-)
+        #m.add_geojson(regions, layer_name="US Regions")
+m.add_points_from_xy(filtered_df, x="lon", y="lat")
+        #m.add_points_from_xy(
+           # cities,
+           # x="lat",
+           # y="lon",
+           # color_column="Projektnummer",
+           # icon_names=["gear", "map", "leaf", "globe"],
+           # spin=True,
+           # add_legend=True,)
 
-st.header("Instructions")
+m.to_streamlit(height=700)
 
-markdown = """
-1. For the [GitHub repository](https://github.com/opengeos/streamlit-map-template) or [use it as a template](https://github.com/opengeos/streamlit-map-template/generate) for your own project.
-2. Customize the sidebar by changing the sidebar text and logo in each Python files.
-3. Find your favorite emoji from https://emojipedia.org.
-4. Add a new app to the `pages/` directory with an emoji in the file name, e.g., `1_🚀_Chart.py`.
-
-"""
-
-st.markdown(markdown)
-
-m = leafmap.Map(minimap_control=True)
-m.add_basemap("OpenTopoMap")
-m.to_streamlit(height=500)
--- pages/3_📍_Marker_Cluster.py
+++ pages/3_📍_Marker_Cluster.py
@@ -1,36 +1,42 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
+import pandas as pd
+
+df = pd.read_csv('data/230710_GeoDatenbank_Lage.csv', sep=';')
+df['lat'] = df['lat'].str.replace(',', '.').astype(float)
+df['lon'] = df['lon'].str.replace(',', '.').astype(float)
+
+projekt_options = ["All"] + list(df['Projektname'].unique())
+selected_projekt = st.selectbox("Select Projektname", projekt_options)
+
+if selected_projekt == "All":
+    filtered_df = df
+else:
+    filtered_df = df[df['Projektname'] == selected_projekt]
+
 
 st.set_page_config(layout="wide")
 
-markdown = """
-A Streamlit map template
-<https://github.com/opengeos/streamlit-map-template>
-"""
 
 st.sidebar.title("About")
-st.sidebar.info(markdown)
-logo = "https://i.imgur.com/UbOXYAU.png"
+logo = r'C:\Users\a.geiger\Documents\GitHub\streamlit_gis\gis.png'
 st.sidebar.image(logo)
 
-st.title("Marker Cluster")
+st.title("Bohrlöcher")
 
-with st.expander("See source code"):
-    with st.echo():
+m = leafmap.Map(center=[54, 15], zoom=4)
+#cities = pd.read_csv('data/230710_GeoDatenbank_Lage.csv', sep=';')
+        #regions = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_regions.geojson"
 
-        m = leafmap.Map(center=[40, -100], zoom=4)
-        cities = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
-        regions = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_regions.geojson"
-
-        m.add_geojson(regions, layer_name="US Regions")
-        m.add_points_from_xy(
-            cities,
-            x="longitude",
-            y="latitude",
-            color_column="region",
-            icon_names=["gear", "map", "leaf", "globe"],
-            spin=True,
-            add_legend=True,
-        )
+        #m.add_geojson(regions, layer_name="US Regions")
+m.add_points_from_xy(filtered_df, x="lon", y="lat")
+        #m.add_points_from_xy(
+           # cities,
+           # x="lat",
+           # y="lon",
+           # color_column="Projektnummer",
+           # icon_names=["gear", "map", "leaf", "globe"],
+           # spin=True,
+           # add_legend=True,)
 
 m.to_streamlit(height=700)
